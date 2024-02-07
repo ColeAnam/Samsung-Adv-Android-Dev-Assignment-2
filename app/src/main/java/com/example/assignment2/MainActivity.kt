a560589d045752a6c43d6d6f83d909a6b8e7a6a9
Cole Anam <coleanam2000@gmail.com>
2024-02-07 14:39:12 -0500

    add input fields

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.assignment2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.assignment2.ui.theme.Assignment2Theme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +42,29 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
 
+    var cName by remember { mutableStateOf("") }
+    var cNo by remember { mutableStateOf("") }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(100.dp)) {
+            TextField(
+                value = cName,
+                onValueChange = { cName = it },
+                label = {
+                    Text("Contact Name")
+                },
+                modifier = Modifier.padding(bottom = 20.dp)
+            )
+            TextField(
+                value = cNo,
+                onValueChange = { cNo = it },
+                label = {
+                    Text("Contact No")
+                },
+                modifier = Modifier.padding(bottom = 20.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
